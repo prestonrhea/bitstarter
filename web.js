a49118c4570537d28bda625875bd7e41ed7d57ae
Preston Rhea <preston.rhea@gmail.com>
2013-07-17 12:43:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buf = new buffer(13);
+var mid = buf.write(fs.readFileSync(index.html,'utf-8'), 0);
+var hello = mid.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
